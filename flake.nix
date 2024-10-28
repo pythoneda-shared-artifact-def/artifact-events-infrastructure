5d2145cb540ca5929c29f1829c787c7f7fc1ecec
rydnr <github@acm-sl.org>
2024-10-28 18:54:30 +0100

    Updated nixpkgs

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     "Shared kernel for pythoneda-shared-artifact/artifact-events-infrastructure";
   inputs = rec {
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
-    nixos.url = "github:NixOS/nixpkgs/23.11";
+    nixos.url = "github:NixOS/nixpkgs/24.05";
     pythoneda-shared-artifact-artifact-events = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
